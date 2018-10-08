6ca0344fca790494f6e2a5da5d46a431b47fdd73
jco-requints <2015081455@ust-ics.mygbiz.com>
2018-10-08 17:06:27 +0800

    Initial commit

---
+++ app/src/main/java/com/magallanes/jerichoisaac/main_activity.java
@@ -0,0 +1,79 @@
+package com.magallanes.jerichoisaac;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1);
+        Log.d("4IT-H", "onCreate() has executed...");
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("4IT-H", "onStart() has executed...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("4IT-H", "onResume() has executed...");
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("4IT-H", "onStop() has executed...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("4IT-H","onDestroy() has executed...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("4IT-H", "onRestart() has executed...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("4IT-H", "onPause() has executed...");
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    public void process(android.view.View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.activity1) {
+
+            i = new Intent(this, activity2.class);
+
+            startActivity(i);
+        }
+        else if(v.getId()==R.id.map){
+            i=new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.553170, 120.980603"));
+
+            chooser= Intent.createChooser(i, "Select Map");
+
+            startActivity(chooser);
+
+        }
+    }
+}
